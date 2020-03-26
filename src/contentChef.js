cff34aaf5ed55e85c31de0ec48f991cd622afb95
PierpaoloIannone <piannone@byte-code.com>
2020-03-26 15:44:26 +0100

    feat: explain contentchef params

--- src/contentChef.js
+++ src/contentChef.js
@@ -1,17 +1,18 @@
 import { configure } from '@contentchef/contentchef-node';
 
 const client = configure({
-    apiKey: '',
-    host: '',
-    spaceId: ''
-}); 
+    apiKey: '<YOUR_API_KEY>',
+    host: '<CONTENTCHEF_HOST>',
+    spaceId: '<YOUR_SPACEID>'
+}, '<TARGETDATE>'); 
 /* 
     CONTENTCHEF_HOST is ContentChef public endpoint or a personal one
-    TARGET_DATE can be a ISO date or a function that returns an ISO date
+    TARGET_DATE (optional) can be a ISO date or a function that returns an ISO date
 */
 
-export const onlineChannel = client.onlineChannel('');
+export const onlineChannel = client.onlineChannel('<YOUR_CHANNEL>');
 // export const previewChannel = client.previewChannel('<YOUR_CHANNEL>', '<PUBLISHING_STATUS>') 
 /* 
-    PUBLISHING_STATUS can either be "live" or "staging"
+    previewChannel can be used to view live/stage contents in a precise date
+    PUBLISHING_STATUS (optional) can either be "live" or "staging"
 */
